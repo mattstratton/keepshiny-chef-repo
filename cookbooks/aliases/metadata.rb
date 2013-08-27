b56adad3ff1fe13e8900e0c924f32730e98f2b71
Matt Stratton <mstratton@apartments.com>
2013-08-27 10:49:05 -0500

    Update with aliases information

---
+++ cookbooks/aliases/metadata.rb
@@ -0,0 +1,8 @@
+name             'aliases'
+maintainer       'Matt Stratton'
+maintainer_email 'matt.stratton@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures aliases'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends           'magic_shell'
